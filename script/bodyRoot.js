7936a5a4e757c839276c601f67570bb730824c18
jeonhyoungmin <jhmin7509@gmail.com>
2022-09-03 15:16:17 +0900

    body, root 값 설정'

---
+++ script/bodyRoot.js
@@ -0,0 +1,8 @@
+export default function bodyRoot(){
+  document.body.style.margin = 0;
+  document.body.style.padding = 0;
+  document.body.style.boxSizing = "border-box"
+  document.body.style.overflowX = "hidden"
+  root.style.width = '100vw';
+  root.style.height = '130vh';
+}
